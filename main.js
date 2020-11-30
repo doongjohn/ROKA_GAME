0667f1f8d88e651e2a3cbc1fbaeb6df4e693dc27
sfun _G <doongjohn@gmail.com>
2020-11-30 14:09:12 +0000

    add screen aspect ratio

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
+var appConfig = {
+  type: Phaser.AUTO,
+  width: 1080,
+  height: 1920,
+  parent: 'phaser-app',
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  },
+  autoRound: false,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 200 }
+    }
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
+};
 
+var game = new Phaser.Game(appConfig);
+
+function preload() {
+  console.log(game.scale);
+}
+
+function create() {
+
+}
+
+function update() {
+
+}
