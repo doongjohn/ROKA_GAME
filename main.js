952dc2eaeb15f8c512961b309a9376714f508b79
sfun _G <doongjohn@gmail.com>
2020-12-07 14:53:26 +0000

    asdasd

--- main.js
+++ main.js
@@ -42,21 +42,59 @@ var appConfig = {
   }
 };
 
+// variables
 var game = new Phaser.Game(appConfig);
+var scene;
+let screenCenterX;
+let screenCenterY;
 
-var screenCenterX;
-var screenCenterY;
+// player
+var player;
+var movementKeys;
+var moveSpeed = 500;
+var moveVec = Phaser.Math.Vector2.ZERO;
+var walkInputDir = Phaser.Math.Vector2.ZERO;
+
+function getWalkDir(inputDir, plusKey, minusKey) {
+  var dir = inputDir;
+  if (!plusKey.isDown && !minusKey.isDown) {
+    return 0;
+  }
+  if (Phaser.Input.Keyboard.JustDown(plusKey) || (plusKey.isDown && Phaser.Input.Keyboard.JustUp(minusKey))) {
+    dir = 1;
+  }
+  if (Phaser.Input.Keyboard.JustDown(minusKey) || (minusKey.isDown && Phaser.Input.Keyboard.JustUp(plusKey))) {
+    dir = -1;
+  }
+  return dir;
+}
+
+function createBouncingRect(posX, posY) {
+  var rec = scene.add.rectangle(posX, posY, 148, 148, 0x6666ff);
+  scene.physics.add.existing(rec);
+  rec.body.velocity.x = 500;
+  rec.body.velocity.y = 500;
+  rec.body.bounce.x = 1;
+  rec.body.bounce.y = 1;
+  rec.body.collideWorldBounds = true;
+}
+
 
 function preload() {
-  // setup
-  screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
-  screenCenterY = this.cameras.main.worldView.y + this.cameras.main.height / 2;
+  scene = this;
+  screenCenterX = scene.cameras.main.worldView.x + scene.cameras.main.width / 2;
+  screenCenterY = scene.cameras.main.worldView.y + scene.cameras.main.height / 2;
+  movementKeys = scene.input.keyboard.addKeys('W,S,A,D');
 }
 
 function create() {
+  // this.input.on('pointerdown', (pointer) => {
+  //   createBouncingRect(pointer.x, pointer.y);
+  // }, this);
+
   var textConfig = {
-    x: screenCenterX,
-    y: screenCenterY,
+    x: 0,
+    y: 0,
     padding: {
       // left: 64,
       // right: 16,
@@ -70,43 +108,30 @@ function create() {
       fontSize: '64px',
       fontFamily: 'Arial',
       color: '#ffffff',
-      align: 'right',  // 'left'|'center'|'right'|'justify'
+      align: 'center',  // 'left'|'center'|'right'|'justify'
       // backgroundColor: '#ff00ff'
     },
     addToScene: true
   };
 
-  // var testText = this.make.text(textConfig);
-  // testText.setPosition(screenCenterX -= (testText.displayWidth / 2), screenCenterY)
-
-  var testText2 = this.add.text(screenCenterX, screenCenterY, '윤재용 일병님 포에버!', {
-    align: 'center',
-    fontSize: '64px',
-    padding: {
-      left: 64,
-      right: 64
-    }
-  });
-  testText2.setPosition(screenCenterX -= (testText2.displayWidth / 2), screenCenterY)
-
   // `this` is a scene object.
-  this.input.on('pointerdown', (pointer) => {
-    createBouncingRect(this, pointer.x, pointer.y);
-  }, this);
+  var testText = this.make.text(textConfig);
+  testText.setPosition(screenCenterX - (testText.displayWidth / 2), screenCenterY)
+  testText.depth = 1;
+
+  // create player
+  player = scene.add.rectangle(screenCenterX, screenCenterY, 148, 148, 0x116fff);
+  scene.physics.add.existing(player);
+  player.body.collideWorldBounds = true;
 }
 
 function update() {
-
+  walkInputDir.y = getWalkDir(walkInputDir.y, movementKeys.S, movementKeys.W);
+  walkInputDir.x = getWalkDir(walkInputDir.x, movementKeys.D, movementKeys.A);
+  moveVec = walkInputDir.clone();
+  moveVec.normalize();
+  moveVec.x *= moveSpeed;
+  moveVec.y *= moveSpeed;
+  player.body.velocity = moveVec;
 }
 
-function createBouncingRect(scene, posX, posY) {
-  var rec1 = scene.add.rectangle(posX, posY, 148, 148, 0x6666ff);
-
-  scene.physics.add.existing(rec1);
-
-  rec1.body.velocity.x = 500;
-  rec1.body.velocity.y = 500;
-  rec1.body.bounce.x = 1;
-  rec1.body.bounce.y = 1;
-  rec1.body.collideWorldBounds = true;
-}
